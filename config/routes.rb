612a72c258d9c998d99e2f8370fdb4a7d148c34c
Francois Villalba <villalbajunior@msn.com>
2023-11-14 16:43:34 +0100

    exo 2 sans optionnel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: 'tasks#index'
+
+  get 'tasks', to: 'tasks#index', as: :index
+
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
